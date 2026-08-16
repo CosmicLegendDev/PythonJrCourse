addf56895b532a945dd14c7a54f24f02f406b0e5
CosmicLegendDev <latchiyaguhanka26@outlook.com>
2026-08-16 11:01:52 -0500

    sa

--- 19-Streamlit/02-medicine-tracker/pages/home.py
+++ 19-Streamlit/02-medicine-tracker/pages/home.py
@@ -6,5 +6,5 @@ st.image("assets/medicine_pic.png")
 profile_name = st.session_state.get("profile_name", "")
 if profile_name == "":
     st.warning("Please enter your name in the profile page to continue.")
-else:
+else:   
     st.success(f"Welcome {profile_name} to the medicine tracker system.")
